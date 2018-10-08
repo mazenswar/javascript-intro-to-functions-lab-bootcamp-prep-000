2df08bc830412219594344ba1d310403b7b45661
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-08 18:42:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function shout(word) {
   return word.toUpperCase();
 }
+
+function whisper(word) {
+  return word.toLowerCase();
+}
+
+function logShout(word) {
+  console.log(word.toUpperCase());
+}
+
+function logWhisper(word) {
+  console.log(word.toLowerCase());
+}
+
+function sayHiToGrandma(word) {
+  if(word === '')
+}
